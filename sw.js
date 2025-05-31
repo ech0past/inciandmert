dbf576deeabc899618da84082c0b04af7fb3d7a3
ech0past <94511910+ech0past@users.noreply.github.com>
2025-05-31 22:38:31 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('push', event => {
-  const data = event.data.json();
-  const title = data.title || 'Bildirim';
-  const options = {
-    body: data.body,
-    icon: data.icon || 'img/pengu.jpg'
-  };
-  event.waitUntil(self.registration.showNotification(title, options));
-});
