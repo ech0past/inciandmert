3ef33205025c8813cba7276ffa7a919c79d564c9
ech0past <94511910+ech0past@users.noreply.github.com>
2025-06-12 01:58:08 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
+self.addEventListener("push", function (event) {
+  const data = event.data.json();
+
+  const options = {
+    body: data.body,
+    icon: data.icon || "img/kedi.gif",
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(data.title || "Bildirim", options)
+  );
 });
