95ed909f2a7611bdcfbd259a6b79cf8be620a06c
ech0past <94511910+ech0past@users.noreply.github.com>
2025-06-13 13:34:14 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener("push", function (event) {
   const data = event.data.json();
   const options = {
     body: data.body,
-    icon: data.icon || "https://ech0past.github.io/img/inciandme.jpg",
+    icon: data.icon || "https://ech0past.github.io/inciandmert/img/inciandme.jpg",
   };
 
   event.waitUntil(
